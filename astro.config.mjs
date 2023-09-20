c707dff07c5eb6ea635e99ef2efa4a507d3d9474
Zhao Youjun <597153372@qq.com>
2023-09-20 16:12:45 +0800

    add 3d scene in homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+//import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zhaomengxiao.github.io",
   base: "/AstroWithThreeJs",
+  //integrations: [react(), tailwind()]
+  integrations: [react()]
 });
